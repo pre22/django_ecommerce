dfdb75035e9041d2ba62cacaa8edb9c5d1b8afe8
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-09-07 08:49:48 +0100

    chores:
    created LoginForm and template,
    created login_page and register_page views/template files,
    Created new routes

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ecommerce.views import home_view, about_view, contact_view
+from ecommerce.views import home_view, about_view, contact_view, login_page, register_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
     path('about/', about_view, name="about"),
-    path('contact/', contact_view, name="contact")
+    path('contact/', contact_view, name="contact"),
+    path('login/', login_page, name="login"),
+    path('register/', register_page, name="register"),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from ecommerce.forms import ContactForm
+from ecommerce.forms import ContactForm, LoginForm
 
 def home_view(request):
     return render(request, "HomePage.html")
@@ -10,9 +10,23 @@ def about_view(request):
 def contact_view(request):
     form = ContactForm(request.POST or None)
 
-    # if form.is_valid():
-
+    if form.is_valid():
+        print(form.cleaned_data)
     context = {
         "form": form
     }
     return render(request, "contact/ContactPage.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/login.html")
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html")
